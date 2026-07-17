26b710b55fe9d061dbae059b4affe59beaeb9aa8
simranjitkaurr <simranjitkaur152017@gmail.com>
2026-07-17 14:41:21 +0530

    Add files via upload

---
+++ JavaScript/01Basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountID="123"
+let accountEmail="abc@gmail.com"
+var accountPassword="5432"
+accountCity="mohali"
+accountState="Punjab";
+
+//accountID=2
+console.log(accountID)
+// node 01Basics/01_variable.js
+console.table({accountID,accountEmail,accountPassword,accountCity,accountState})
---
+++ JavaScript/01Basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict" // treat all js code as newer version
+
+//alert(3+3) // 6
+
+console.log(3+3) 
+
+let name="simi"
+let age= 22
+let isLoggedIn= false
+let state; //undefined
+let a=null
+
+//console.table({name,age,isLoggedIn,state,a})
+
+// number= 2 to power 53 memory address
+// bigint
+//boolean true/false
+// string
+//null standalone
+//undefined 
+//symbol =uniqueness
+
+
+//object
+console.log(typeof name)
+
+console.log(typeof undefined)// undefined
+console.log(typeof null) // object
+
+
+
+
